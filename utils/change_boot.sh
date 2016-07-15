66f08ec7f9729db820660d0c766de3bf23db5425
root <root@cz7828.bud.mirantis.net>
2016-07-15 14:35:34 +0200

    Changed boot options which needs to be modified

--- utils/change_boot.sh
+++ utils/change_boot.sh
@@ -21,26 +21,26 @@
 change_boot()
 {
 BOOT_PARTITION_STATE=`mount | awk '{print $3}' | grep "/boot"`
-if [ "${BOOT_PARTITION_STATE}" != "" ]; then
-        echo "[INFO]: Boot partition exists. Changing boot partition options:"
-	sed -i "s/${LV_CURRENT_ROOT_UUID}/${LV_FUTURE_ROOT_UUID}/g" /boot/grub/grub.cfg
-	if [ $? -eq 0 ]; then
-        	echo "[INFO]: UUID info in /boot/grub/grub.cfg file has been changed."
-	else
-	        echo "[ERROR]: UUID info in /boot/grub/grub.cfg file hasn't been changed."
-	        exit
-	fi
-
-	sed -i "s/${LV_CURRENT_ROOT}/${LV_FUTURE_ROOT}/g" /boot/grub/grub.cfg
-	if [ $? -eq 0 ]; then
-	        echo "[INFO]: Information regarding ${LV_FUTURE_ROOT} has been added in /boot/grub/grub.cfg file."
-	else
-	        echo "[ERROR]: Information regarding ${LV_FUTURE_ROOT} hasn't been added in /boot/grub/grub.cfg file."
-	        exit
-	fi
-
+#if [ "${BOOT_PARTITION_STATE}" != "" ]; then
+echo "[INFO]: Changing boot options:"
+sed -i "s/${LV_CURRENT_ROOT_UUID}/${LV_FUTURE_ROOT_UUID}/g" /boot/grub/grub.cfg
+if [ $? -eq 0 ]; then
+	echo "[INFO]: UUID info in /boot/grub/grub.cfg file has been changed."
 else
-        echo "[INFO]: Boot partition doesn't exist. Changing boot partition options:"i
+        echo "[ERROR]: UUID info in /boot/grub/grub.cfg file hasn't been changed."
+        exit
+fi
+
+sed -i "s/${LV_CURRENT_ROOT}/${LV_FUTURE_ROOT}/g" /boot/grub/grub.cfg
+if [ $? -eq 0 ]; then
+        echo "[INFO]: Information regarding ${LV_FUTURE_ROOT} has been added in /boot/grub/grub.cfg file."
+else
+     	echo "[ERROR]: Information regarding ${LV_FUTURE_ROOT} hasn't been added in /boot/grub/grub.cfg file."
+        exit
+fi
+
+if [ "${BOOT_PARTITION_STATE}" = "" ]; then
+        echo "[INFO]: Boot partition doesn't exist. Changing boot options on the destination file system as well:"
         sed -i "s/${LV_CURRENT_ROOT_UUID}/${LV_FUTURE_ROOT_UUID}/g" ${BASE_DIR}/mnt/destination_fs/boot/grub/grub.cfg
         if [ $? -eq 0 ]; then
                 echo "[INFO]: UUID info in ${BASE_DIR}/mnt/destination_fs/boot/grub/grub.cfg file has been changed."
@@ -56,7 +56,5 @@ else
                 echo "[ERROR]: Information regarding ${LV_FUTURE_ROOT} hasn't been added in ${BASE_DIR}/mnt/destination_fs/boot/grub/grub.cfg file."
                 exit
         fi
-
-
 fi
 }
